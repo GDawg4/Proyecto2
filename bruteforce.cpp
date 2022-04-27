1636a5e1b2a0bf7215346d75830a41bb1cc7b931
Rodrigo Garoz <35437197+GDawg4@users.noreply.github.com>
2022-04-26 23:45:03 -0600

    Adding programs

---
+++ bruteforce.cpp
@@ -0,0 +1,138 @@
+#include "cryptopp/osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <cstdlib>
+using std::exit;
+
+#include "cryptopp/cryptlib.h"
+using CryptoPP::Exception;
+
+#include "cryptopp/hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "cryptopp/filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::StreamTransformationFilter;
+
+#include "cryptopp/des.h"
+using CryptoPP::DES;
+
+#include "cryptopp/modes.h"
+using CryptoPP::CBC_Mode;
+
+#include "cryptopp/secblock.h"
+using CryptoPP::SecByteBlock;
+
+int main(int argc, char* argv[])
+{
+	AutoSeededRandomPool prng;
+
+	SecByteBlock key(DES::DEFAULT_KEYLENGTH);
+	prng.GenerateBlock(key, key.size());
+
+	CryptoPP::byte iv[DES::BLOCKSIZE];
+	prng.GenerateBlock(iv, sizeof(iv));
+
+	string plain = "CBC Mode Test";
+	string cipher, encoded, recovered;
+
+	/*********************************\
+	\*********************************/
+
+    // cout << "key length: " << DES::DEFAULT_KEYLENGTH << endl;
+    // cout << "block size: " << DES::BLOCKSIZE << endl;
+
+	// Pretty print key
+	encoded.clear();
+	StringSource(key, key.size(), true,
+		new HexEncoder(
+			new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+	cout << "key: " << encoded << endl;
+
+	// Pretty print iv
+	encoded.clear();
+	StringSource(iv, sizeof(iv), true,
+		new HexEncoder(
+			new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+	cout << "iv: " << encoded << endl;
+
+	/*********************************\
+	\*********************************/
+
+	try
+	{
+		cout << "plain text: " << plain << endl;
+
+		CBC_Mode< DES >::Encryption e;
+		e.SetKeyWithIV(key, key.size(), iv);
+
+		// The StreamTransformationFilter adds padding
+		//  as required. ECB and CBC Mode must be padded
+		//  to the block size of the cipher.
+		StringSource(plain, true, 
+			new StreamTransformationFilter(e,
+				new StringSink(cipher)
+			) // StreamTransformationFilter      
+		); // StringSource
+	}
+	catch(const CryptoPP::Exception& e)
+	{
+		cerr << e.what() << endl;
+		exit(1);
+	}
+
+	/*********************************\
+	\*********************************/
+
+	// Pretty print
+	encoded.clear();
+	StringSource(cipher, true,
+		new HexEncoder(
+			new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+	cout << "cipher text: " << encoded << endl;
+
+	/*********************************\
+	\*********************************/
+
+	try
+	{
+		CBC_Mode< DES >::Decryption d;
+		d.SetKeyWithIV(key, key.size(), iv);
+
+		// The StreamTransformationFilter removes
+		//  padding as required.
+		StringSource s(cipher, true, 
+			new StreamTransformationFilter(d,
+				new StringSink(recovered)
+			) // StreamTransformationFilter
+		); // StringSource
+
+		cout << "recovered text: " << recovered << endl;
+	}
+	catch(const CryptoPP::Exception& e)
+	{
+		cerr << e.what() << endl;
+		exit(1);
+	}
+
+	/*********************************\
+	\*********************************/
+
+	return 0;
+}
